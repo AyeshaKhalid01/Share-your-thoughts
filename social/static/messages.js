a0e1eaa2f76cfb4ef6388a0a203fb369411fb738
Ayesha Khalid <khalia34@mcmaster.ca>
2020-08-17 21:21:37 -0400

    static

---
+++ social/static/messages.js
@@ -0,0 +1,68 @@
+/* ********************************************************************************************
+   | Handle Submitting Posts - called by $('#post-button').click(submitPost)
+   ********************************************************************************************
+   */
+function submitPost(event) {
+    let postContent=$('#post-text').text();
+    // TODO Objective 8: send contents of post-text via AJAX Post to post_submit_view (reload page upon success)
+    let json_data = { 'postContent' : postContent };
+    // globally defined in messages.djhtml using i{% url 'social:like_view' %}
+    let url_path = post_submit_url;
+    // AJAX post
+    $.post(url_path,
+           json_data, moreResponse);
+}
+
+/* ********************************************************************************************
+   | Handle Liking Posts - called by $('.like-button').click(submitLike)
+   ********************************************************************************************
+   */
+function submitLike(event) {
+   let postID = this.id;
+    //alert('Like Button Pressed');
+    // TODO Objective 10: send post-n id via AJAX POST to like_view (reload page upon success)
+   let json_data = { 'postID' : postID };
+   let url_path = like_post_url;
+    // AJAX post
+    $.post(url_path,
+           json_data, moreResponse);
+}
+
+/* ********************************************************************************************
+   | Handle Requesting More Posts - called by $('#more-button').click(submitMore)
+   ********************************************************************************************
+   */
+function moreResponse(data,status) {
+    if (status == 'success') {
+        // reload page to display new Post
+        location.reload();
+    }
+    else {
+        alert('failed to request more posts' + status);
+    }
+}
+
+function submitMore(event) {
+    // submit empty data
+    let json_data = { };
+    // globally defined in messages.djhtml using i{% url 'social:more_post_view' %}
+    let url_path = more_post_url;
+
+    // AJAX post
+    $.post(url_path,
+           json_data,
+           moreResponse);
+}
+
+/* ********************************************************************************************
+   | Document Ready (Only Execute After Document Has Been Loaded)
+   ********************************************************************************************
+   */
+$(document).ready(function() {
+    // handle post submission
+    $('#post-button').click(submitPost);
+    // handle likes
+    $('.like-button').click(submitLike);
+    // handle more posts
+    $('#more-button').click(submitMore);
+});
