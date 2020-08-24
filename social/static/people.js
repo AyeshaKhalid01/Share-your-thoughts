b1993b7ac108b6aad8459c115cc642b8381b1c1b
AyeshaKhalid01 <56558665+AyeshaKhalid01@users.noreply.github.com>
2020-08-24 19:18:38 -0400

    Update people.js

--- social/static/people.js
+++ social/static/people.js
@@ -1,1 +1,13 @@
-
+/* ********************************************************************************************
+   | Handle Submiting Friend Requests - called by $('.like-button').click(submitLike)
+   ********************************************************************************************
+   */
+function frResponse(data,status) {
+    if (status == 'success') {
+        // reload page to update like count
+        location.reload();
+    }
+    else {
+        alert('failed to create friend request ' + status);
+    }
+}
