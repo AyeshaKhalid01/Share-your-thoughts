c2a32740756be4fba651c0e7fa9ed8b6389ad8ef
AyeshaKhalid01 <56558665+AyeshaKhalid01@users.noreply.github.com>
2020-09-16 14:11:25 -0400

    Update urls.py

--- login/urls.py
+++ login/urls.py
@@ -13,3 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path
+from . import views
+
+app_name = 'login'
+urlpatterns = [
+    path('', views.login_view,name='login_view'),
+    path('logout/', views.logout_view,name='logout_view'),
+    path('signup/', views.signup_view,name='signup_view'),
+]
